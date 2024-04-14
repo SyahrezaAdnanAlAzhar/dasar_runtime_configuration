11f3aa3856740e610af340e7164ba2a8d4aeb096
Syahreza Adnan Al Azhar <notsupersyahreza@student.telkomuniversity.ac.id>
2024-04-14 22:24:57 +0700

    menyelesaikan program dengan runtime configuration

--- program_runtime_cofiguration/CovidConfig.cs
+++ program_runtime_cofiguration/CovidConfig.cs
@@ -4,20 +4,20 @@ using System.Text.Json;
 
 public class CovidConfig
 {
-	public string satuan_suhu { get; set; }
-	public int batas_hari_demam {  get; set; }
-	public string pesan_ditolak {  get; set; }
-	public string pesan_diterima {  get; set; }
+    public string satuan_suhu { get; set; }
+    public int batas_hari_demam { get; set; }
+    public string pesan_ditolak { get; set; }
+    public string pesan_diterima { get; set; }
 
-	public CovidConfig()
-	{
-		satuan_suhu = "celcius";
-		batas_hari_demam = 14;
-		pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
-		pesan_diterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+    public CovidConfig()
+    {
+        satuan_suhu = "celcius";
+        batas_hari_demam = 14;
+        pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+        pesan_diterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
     }
 
-    public void ReadFileConfig(string path)
+    public void ReadConfigFile(string path)
     {
         if (File.Exists(path))
         {
@@ -34,11 +34,24 @@ public class CovidConfig
         }
     }
 
-    public void WriteFileConfig(string path)
+    public void WriteConfigFile(string path)
     {
         string jsonText = JsonSerializer.Serialize(this);
         File.WriteAllText(path, jsonText);
         Console.WriteLine("File config berhasil ditulis.");
+    }
+
+    public void ReadAndWriteConfigFile(string path)
+    {
+        if (File.Exists(path))
+        {
+            ReadConfigFile(path);
+        }
+        else
+        {
+            WriteConfigFile(path);
+            ReadConfigFile(path);
+        }
     }
 
     public void UbahSatuan()
@@ -51,5 +64,26 @@ public class CovidConfig
         {
             this.satuan_suhu = "celcius";
         }
+    }
+
+    public bool kondisiSuhu(double suhuBadan)
+    {
+        if (this.satuan_suhu == "celcius" && suhuBadan >= 36.5 && suhuBadan <= 37.5)
+        {
+            return true;
+        }else if (this.satuan_suhu == "fahrenheit" && suhuBadan >= 97.7 && suhuBadan <= 99.5)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool kondisiHariDemam(int hariDemam)
+    {
+        if (hariDemam < this.batas_hari_demam)
+        {
+            return true;
+        }
+        return false;
     }
 }
--- program_runtime_cofiguration/Program.cs
+++ program_runtime_cofiguration/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CovidConfig cvdConfig = new CovidConfig();
+
+        string configPath = "covid_config_1302223041.json";
+        
+        cvdConfig.ReadAndWriteConfigFile(configPath);
+
+        Console.Write($"Berapa suhu badan anda saat ini? Dalam nilai {cvdConfig.satuan_suhu}: ");
+
+        double suhuBadan = double.Parse(Console.ReadLine());
+
+        Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        int hariTerakhirDemam = int.Parse(Console.ReadLine());
+
+        if (cvdConfig.kondisiSuhu(suhuBadan) && cvdConfig.kondisiHariDemam(hariTerakhirDemam))
+        {
+            Console.WriteLine(cvdConfig.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(cvdConfig.pesan_ditolak);
+        }
+
+        cvdConfig.UbahSatuan();
+
+        Console.Write($"Berapa suhu badan anda saat ini? Dalam nilai {cvdConfig.satuan_suhu}: ");
+
+        double suhuBadan1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        int hariTerakhirDemam1 = int.Parse(Console.ReadLine());
+
+        if (cvdConfig.kondisiSuhu(suhuBadan1) && cvdConfig.kondisiHariDemam(hariTerakhirDemam1))
+        {
+            Console.WriteLine(cvdConfig.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(cvdConfig.pesan_ditolak);
+        }
+
+        string newJsonText = JsonSerializer.Serialize(cvdConfig);
+        File.WriteAllText(configPath, newJsonText);
+    }
+}
